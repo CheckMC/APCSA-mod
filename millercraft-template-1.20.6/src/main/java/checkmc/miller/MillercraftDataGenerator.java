bace7094cd585aca166364773d94800b2ef62e86
Check <71415887+CheckMC@users.noreply.github.com>
2024-05-09 11:08:38 -0400

    CREATE
    MOD MOD MOD MOD MOD

---
+++ millercraft-template-1.20.6/src/main/java/checkmc/miller/MillercraftDataGenerator.java
@@ -0,0 +1,11 @@
+package checkmc.miller;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MillercraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
